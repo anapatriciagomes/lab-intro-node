038756dc784405a341df254a5f6c756fef111935
anapatriciagomes <anapatrigomes@gmail.com>
2024-01-31 20:59:10 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    function compareNumbers(a, b) {
+      return a - b;
+    }
+    this.items.sort(compareNumbers);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => Math.max(a, b), -Infinity);
+    } else {
+      throw new Error('Empty SortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => Math.min(a, b), +Infinity);
+    } else {
+      throw new Error('Empty SortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b);
+    } else {
+      return 0;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      return this.items.reduce((a, b) => a + b) / this.items.length;
+    } else {
+      throw new Error('Empty SortedList');
+    }
+  }
 }
 
 module.exports = SortedList;
